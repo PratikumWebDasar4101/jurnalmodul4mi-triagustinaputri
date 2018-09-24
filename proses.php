743895f334531a19a96159e87fc457aafeaf6181
triagustinaputri <42725273+triagustinaputri@users.noreply.github.com>
2018-09-24 10:50:35 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,3 +1,19 @@
 <?php
+//checkbox
+$hobbi = $_POST['hobbi'];
+echo "hobbi Anda :";
+foreach ($hobbi as $key => $value) {
+	echo "$value <br>";
+}
+print_r($hobbi);
 
+//gambar foto
+$file = $_FILES['dokumen'];  //digunakan untuk mengambil file mentahnya yang kemudian dimasukan dalam variabel $file.
+$nama_file = $file['name']; //Untuk memperoleh nama file digunakan script
+$nama_tmp = $file['tmp_name']; //Untuk mendapatkan nama temporer 
+$upload_dir = "upload/"; //adalah folder tempat diletakkannya file yang akan diupload.
+move_uploaded_file($nama_tmp,$upload_dir.$nama_file);
+echo "File berhasil diunggah. <br>"
 ?>
+<img src="upload/<?php echo $nama_file; ?>">
+
